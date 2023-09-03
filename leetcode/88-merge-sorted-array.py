63844483f872fb081c582ef6f248acf217f15b70
Josh Blatt <thejoshblatt@yahoo.com>
2023-09-03 11:28:01 -0400

    merge sorted array O(1) space

--- leetcode/88-merge-sorted-array.py
+++ leetcode/88-merge-sorted-array.py
@@ -13,3 +13,18 @@ class Solution:
                 nums1[write_pointer] = nums2[read_pointer2]
                 read_pointer2 += 1
             write_pointer += 1
+
+# O(m+n) time O(1) space
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        p1 = m - 1
+        p2 = n - 1
+        for p in range(m + n - 1, -1, -1):
+            if p2 < 0:
+                break
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
