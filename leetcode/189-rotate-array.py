ca0a1e9299d347825830672fa69fc8571da43cf6
Josh Blatt <thejoshblatt@yahoo.com>
2023-09-04 10:00:44 -0400

    adding better solution to rotate array

--- leetcode/189-rotate-array.py
+++ leetcode/189-rotate-array.py
@@ -18,3 +18,21 @@ class Solution:
 # reverse last k numbers
 # 5 6 7 1 2 3 4
 # O(n) time O(1) space
+class Solution:
+    def reverse(self, nums, start, end):
+        while start < end:
+            temp = nums[start]
+            nums[start] = nums[end]
+            nums[end] = temp
+            start += 1
+            end -= 1
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k %= n
+        self.reverse(nums, 0, n - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, n - 1)
+        
