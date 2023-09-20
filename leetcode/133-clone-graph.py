c870d82b1dc6088c4ce249d46ab55e1a04c78920
Josh Blatt <thejoshblatt@yahoo.com>
2023-09-20 13:56:30 -0400

    removing dup

--- leetcode/133-clone-graph.py
+++ leetcode/133-clone-graph.py
@@ -29,35 +29,4 @@ class Solution:
             clone_node.neighbors = [self.cloneGraph(n) for n in node.neighbors]
     
         return clone_node
-        """
-# Definition for a Node.
-class Node:
-    def __init__(self, val = 0, neighbors = None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
-"""
-
-from typing import Optional
-class Solution:
-    def __init__(self):
-        # key = original node, value = cloned node
-        self.visited = {}
-
-    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        if not node:
-            return node
-
-        # if clone already exists return it
-        if node in self.visited:
-            return self.visited[node]
-
-        # clone doesn't exist, so make it
-        clone_node = Node(node.val, [])
-        self.visited[node] = clone_node
-
-        if node.neighbors:
-            # clone neighbors if necessary, add them to cloned node's neighbors
-            clone_node.neighbors = [self.cloneGraph(n) for n in node.neighbors]
-    
-        return clone_node
         
