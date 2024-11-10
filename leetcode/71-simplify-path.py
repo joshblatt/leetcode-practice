1b3c4fee44c6fe0318ed336ecbdd7210526ffb61
Josh Blatt <thejoshblatt@yahoo.com>
2024-11-10 12:20:19 -0800

    simplify path

--- leetcode/71-simplify-path.py
+++ leetcode/71-simplify-path.py
@@ -1,19 +1,13 @@
 # O(n) time O(n) space
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        path = path.split('/')
         stack = []
-        new_path = ''
-        for i in range(len(path)):
-            if path[i] == '' or path[i] == '.':
-                continue
-            elif path[i] == '..':
-                if len(stack) > 0:
+        path = path.split('/')
+        for directory in path:
+            if directory == '..':
+                if stack:
                     stack.pop()
-            else:
-                stack.append(path[i])
-        for i in range(len(stack)):
-            new_path += ('/' + stack[i])
-        if len(new_path) == 0:
-            new_path += '/'
-        return new_path
+            elif directory and directory != '.':
+                stack.append(directory)
+
+        return "/" + "/".join(stack) if stack else '/'
